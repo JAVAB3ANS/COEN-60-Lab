25901a8d1812a05a877d9f0ee925baf215249af6
Jason Vu <jvu@scu.edu>
2021-12-05 18:10:06 -0800

    fix lab 9 php

--- LAB-9/register.php
+++ LAB-9/register.php
@@ -1,26 +1,30 @@
 <?php 
-    $account= $_POST["account"];
-    $usernamelength= strlen($account); 
-    $password= $_POST["password"];
     $existingAccountsArray= array("coen60","coen60l"); 
+    $errorCount = 0;
     
     if (isset($_POST["enterButton"])) { 
-        if (empty($account)) {
-            echo "<br>Invalid response. You forgot to enter an account name!"; 
+        if (empty($_POST["account"])) {
+            echo "<br>Invalid response. You forgot to enter an account name!";
+            $errorCount++;
         }
-        else if (empty($_POST["password"])) {
-            echo "<br>Invalid response. You forgot to enter a password!"; 
+        if (empty($_POST["password"])) {
+            echo "<br>Invalid response. You forgot to enter a password!";
+            $errorCount++;
         }
-        else if (strlen($password) < 8) {
-            echo "<br>Invalid password. Password must be at least 8 characters"; 
+        if (strlen($_POST["account"]) < 8) {
+            echo "<br>Invalid password. Password must be at least 8 characters";
+            $errorCount++;
         } 
-        else if (!preg_match("~[0-9]+~", $password)) {
-            echo "<br>Invalid password. Password must contain at least 1 digit."; 
+        if (!preg_match("~[0-9]+~", $_POST["password"])) {
+            echo "<br>Invalid password. Password must contain at least 1 digit.";
+            $errorCount++;
         }   
-        else if (in_array($account, $existingAccountsArray)) {
+        if (in_array($_POST["account"], $existingAccountsArray)) {
             echo "<br>Account can't match existing accounts.";
-        }  else {
-            echo "<br>Success!";
+            $errorCount++;
+        }  
+        if ($errorCount === 0 ) {
+            echo ("<br>Success!");
         }
     }
 ?>
